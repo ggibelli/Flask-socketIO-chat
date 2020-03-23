aec623201fefcfa5e0e7a0fe118345cfea826407
ggibelli <giogibelli@icloud.com>
2020-03-23 17:57:28 +1300

    delete message frontend

--- application.py
+++ application.py
@@ -5,21 +5,63 @@ import json
 from flask import Flask, jsonify, render_template, request
 from flask_socketio import SocketIO, emit, send, join_room, leave_room
 from collections import defaultdict
-
+import time
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+messageID = 0
+users_online = {}
 channels = []
-active_users = set()
-active_users_channel = defaultdict(list)
-messages = []
+active_users = {}
+reload_handler = {}
+disconnection_handler = {}
+
 messages_channel = defaultdict(list)
 
 @app.route("/")
 def index():
     return render_template("index.html", channels = channels)
+
+# Handling client connection and sending list to frontend 
+@socketio.on('connected')
+def connected(data):
+    username = data['username']
+    now = time.time()
+    user_sid = request.sid
+    users_online[username] = user_sid
+    reload_handler[username] = now
+    disconnection_handler[user_sid] = username
+    emit('onlineusers', {'onlineusers': users_online}, broadcast=True)
+
+# Handling disconnection and page reload
+@socketio.on('disconnect')
+def test_disconnect():
+    user_sid = request.sid
+    now = time.time()
+    print('##########')
+    print(now)
+    for k, v in list(users_online.items()):
+        if v == user_sid:
+            del users_online[k]
+    if user_sid in disconnection_handler:
+        user_left = disconnection_handler[user_sid]
+        channel_left = active_users[user_left]
+        times = [v for k,v in reload_handler.items() if k == user_left]
+        print(times[0])
+        print(reload_handler)
+        if now - times[0] > 70:
+            del active_users[user_left]
+            emit('leaveuser', {'user': user_left}, room=channel_left)
+            emit('disconnected', {'user': user_left, 'usersid': user_sid}, broadcast=True)
+        #channel_left = active_users[user_left]
+        #if user_left not in users_online and now - reload_handler[user_sid][1] > 180:
+            #diocaneeeeee
+        #if now - reload_handler[user_sid][1] > 90:
+            #emit('leaveuser', {'user': user_left}, room=channel_left)
+            #emit('disconnected', {'user': user_left, 'usersid': user_sid}, broadcast=True)
+            #emit('announcement', {'message': user_left + ' has left the room.'}, room=channel_left) 
 
 @socketio.on("create channel")
 def create(data):
@@ -32,29 +74,23 @@ def on_join(data):
     username = data['username']
     room = data['channel']
     join_room(room)
-    active_users.add((room, username))
-    for channel, user in active_users:
-        if user not in active_users_channel[channel]:
-            active_users_channel[channel].append(user)
-    emit('announcement', {'message': username + ' has entered the room.'}, room=room)
-    emit('joinuser', {'userlist': active_users_channel[room]}, room=room)
+    active_users[username] = room
+    userlist = [k for k,v in active_users.items() if v == room]
+    #emit('announcement', {'message': username + ' has entered the room.'}, room=room)
+    emit('joinuser', {'userlist': userlist, 'messages': messages_channel[room]}, room=room)
 
 @socketio.on('leave')
 def on_leave(data):
     username = data['username']
     room = data['channel']
-    leave_room(room)
-    for channel, user in active_users:
-        if user in active_users_channel[channel]:
-            active_users_channel[channel].remove(user)
-    active_users.discard((room, username))    
+    leave_room(room)   
     emit('leaveuser', {'user': username}, room=room)
-    emit('announcement', {'message': username + ' has left the room.'}, room=room)
+    #emit('announcement', {'message': username + ' has left the room.'}, room=room)
     
 @socketio.on("send message")
-
 def message(data):
-    print(data['JSON']['message'])
+    messages = []
+    messageID += 1
     room = data['channel']
     username = data['JSON']['user']
     date = data['JSON']['date']
@@ -62,8 +98,14 @@ def message(data):
     messages.append((room, data['JSON']))
     for channel, message in messages:
         messages_channel[channel].append(message)
-    print(messages_channel)
-    emit('broadcast message', {'message': message, 'username': username, 'date': date}, room=room)
+    emit('broadcast message', {'message': message, 'username': username, 'date': date, 'messageID': messageID}, room=room)
+
+@socketio.on("delete message")
+def delete(data):
+    user = data['username']
+    room = data['channel']
+    message = data['message']
+    date = data['date']
 
 if __name__ == "__main__":
     socketio.run(app)
--- static/script.js
+++ static/script.js
@@ -1,202 +1,281 @@
 const formchannel = document.querySelector('#CreateChannel');
 let username; 
 let channels = [];
-let users = [];
+let users_channel = [];
+let users_online = {};
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Connect to websocket
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+  // Connect to websocket
+  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-    // Retrieve username from localstorage
-    if (!localStorage.getItem('nickname')) {
-        username = window.prompt("Enter you nickname","username" + (Math.ceil(Math.random() * 100)));
-        localStorage.setItem('nickname', username)
+  // Retrieve username from localstorage
+  if (!localStorage.getItem('nickname')) {
+    username = window.prompt("Enter you nickname","username" + (Math.ceil(Math.random() * 100)));
+    localStorage.setItem('nickname', username)
+  }
+  username = localStorage.getItem('nickname')
+
+  // Retrieve last channel and username
+  document.querySelectorAll('#Channels li a').forEach(function(channel){
+    channels.push((channel.innerHTML));
+  })
+  if (!localStorage.getItem('lastchannel') || !channels.includes(localStorage.getItem('lastchannel'))) {
+    lastchannel = "general";
+    localStorage.setItem('lastchannel', lastchannel);
+  }
+
+  // Send connection notice to server
+  socket.emit('connected', {'username': username});
+
+  // Join last channel
+  joinChannel(localStorage.getItem('lastchannel'));
+
+  // Create list of online users
+  socket.on('onlineusers', data => {
+    const item = document.querySelector('#onlineUsers')
+    while (item.firstChild) {
+      item.removeChild(item.firstChild)
     }
-    username = localStorage.getItem('nickname')
-
-    // Retrieve last channel
-    document.querySelectorAll('#Channels li a').forEach(function(channel){
-        channels.push((channel.innerHTML));
-    })
-
-    if (!localStorage.getItem('lastchannel') || !channels.includes(localStorage.getItem('lastchannel'))) {
-        lastchannel = "general";
-        localStorage.setItem('lastchannel', lastchannel);
+    let entries = Object.entries(data.onlineusers)
+    for (let [user, sid] of entries) {
+      users_online[user] = sid; 
+      const li = document.createElement('li');
+      li.setAttribute('id', sid)
+      li.setAttribute('class', 'list-group-item')
+      const a = li.appendChild(document.createElement('a'));        
+      a.innerHTML = user;
+      a.href = "#";
+      document.querySelector('#onlineUsers').append(li);
     }
+    console.log(users_online)
+  })
 
-    username = localStorage.getItem('nickname')
-    joinChannel(localStorage.getItem('lastchannel'));
+  // Update the user online list
+  socket.on('disconnected', data => {
+    console.log(data)
+    console.log(users_online)
+    delete users_online[data.user]
+    document.querySelector(`#${CSS.escape(data.usersid)}`).remove()
+    //document.querySelector('#'+data.usersid).remove()
+  })
 
-    // Logic form channel creation
-    // By default, submit button is disabled
-    document.querySelector('#submit').disabled = true;
 
-    // Enable button only if there is text in the input field
-    document.querySelector('#Channel').onkeyup = () => {
-        if (document.querySelector('#Channel').value.length > 0) {
-            document.querySelector('#submit').disabled = false;
-        }
-        else {
-            document.querySelector('#submit').disabled = true;
-        }
+  // Logic form channel creation
+  // By default, submit button is disabled
+  document.querySelector('#submit').disabled = true;
+
+  // Enable button only if there is text in the input field
+  document.querySelector('#Channel').onkeyup = () => {
+    if (document.querySelector('#Channel').value.length > 0) {
+      document.querySelector('#submit').disabled = false;
     }
+    else {
+      document.querySelector('#submit').disabled = true;
+    }
+  }
 
-    socket.on('leaveuser', data => {
-        console.log(data.user)
-        users.splice(users.indexOf(data.user))
-        let li = document.getElementById(data.user);
-        li.remove(); 
-    })
+  // When an user leaves update the user channel list
+  socket.on('leaveuser', data => {
+    users_channel.splice(users_channel.indexOf(data.user))
+    let li = document.getElementById(data.user);
+    if (li) {
+      li.remove(); 
+    }
+  })
 
-    socket.on('joinuser', data => {
-        let length = data.userlist.length;
-        console.log(data.userlist)
-        let userlist = document.querySelectorAll('#userList li');
-        for(let i=0; li=userlist[i]; i++) {
-            li.parentNode.removeChild(li);
-        }
-        for (let i = 0; i < length; i++){
-            if (!users.includes(data.userlist[i])){
-                users.push(data.userlist[i]);
-            }
-        }
-        console.log(users)
-        for (user of users){
-            const li = document.createElement('li');
-            li.setAttribute('id', user)
-            const a = li.appendChild(document.createElement('a'));        
-            a.innerHTML = user;
-            a.href = "#";
-            document.querySelector('#userList').append(li); 
-        }
-    })
+  // When an user joins update the user channel list
+  socket.on('joinuser', data => {
+    //console.log(data.messages[0].message)
+    const item = document.querySelector('#SingleMessage')
+    while (item.firstChild) {
+      item.removeChild(item.firstChild)
+    }
+    let messagelength = data.messages.length;
+    if (messagelength > 100){
+      messagelength = 99;
+    }
+    for (let i = 0; i < messagelength; i++) {
+      appendMessage(data.messages[i].message, data.messages[i].user, data.messages[i].date)
+    }
+    let userlength = data.userlist.length;
+    let userlist = document.querySelectorAll('#userList li');
+    for(let i=0; li=userlist[i]; i++) {
+      li.parentNode.removeChild(li);
+    }
+    for (let i = 0; i < userlength; i++){
+      if (!users_channel.includes(data.userlist[i])){
+        users_channel.push(data.userlist[i]);
+      }
+    }
+    for (user of users_channel){
+      const li = document.createElement('li');
+      li.setAttribute('id', user)
+      const a = li.appendChild(document.createElement('a'));        
+      a.innerHTML = user;
+      a.href = "#";
+      document.querySelector('#userList').append(li); 
+    }
+  })
 
-    // When connected, configure buttons
-    socket.on('connect', () => {
+  // When connected, configure buttons
+  socket.on('connect', () => {
 
-        formchannel.addEventListener('submit', event => {
+    formchannel.addEventListener('submit', event => {
 
-            // Create new item for list
-            const channel = document.querySelector('#Channel').value;
-            if (!channels.includes(channel)) {
-                socket.emit('create channel', {'channel': channel});
-                actualchannel = localStorage.getItem('lastchannel');
-                channels.push(channel);
-                if (checkChannel(channel, actualchannel)){
-                    leaveChannel(actualchannel);
-                    joinChannel(channel);
-                }
-            }
-            else {
-                window.alert("The channel already exists");
-                return false;
-            }
-            // Clear input field and disable button again
-            document.querySelector('#Channel').value = '';
-            document.querySelector('#submit').disabled = true;
-
-            // Stop form from submitting
-            event.preventDefault();  
-        })
-    })
-
-    socket.on('new channel', data => {
-        channel = data.channel;
+      // Create new item for list
+      const channel = document.querySelector('#Channel').value;
+      if (!channels.includes(channel)) {
+        socket.emit('create channel', {'channel': channel});
+        actualchannel = localStorage.getItem('lastchannel');
         channels.push(channel);
-        const li = document.createElement('li');
-        li.setAttribute('class', 'list-group-item active')
-        const a = li.appendChild(document.createElement('a'));        
-        a.innerHTML = channel;
-        a.href = "#";
-        a.addEventListener("click", () => { 
-            actualchannel = localStorage.getItem('lastchannel');
-            newchannel = a.innerHTML;
-            if (checkChannel(actualchannel, newchannel)){
-                leaveChannel(actualchannel);
-                socket.on('leaveuser', data => {
-                    console.log(data.user)
-                    let li = document.getElementById(data.user);
-                    if (li) {
-                        li.remove(); 
-                    }
-                })
-                joinChannel(newchannel);
-                
-                return false;
-            }   
-        })
-        document.querySelector('#Channels').append(li);
-    });
-
-    socket.on('announcement', data =>{
-        window.alert(data.message);
-    })
-
-    // When a channel is clicked, connect to that channel
-    document.querySelectorAll('#Channels li').forEach(function(channel){
-        channels.push(channel.innerHTML);
-        channel.onclick = () =>{
-            document.querySelector('.list-group-item.active').classList.remove('active');
-            let chanlist = channel.classList;
-            chanlist.add('active');
-            actualchannel = localStorage.getItem('lastchannel');
-            newchannel = channel.innerHTML;
-            if (checkChannel(newchannel, actualchannel)){
-                leaveChannel(actualchannel);
-                joinChannel(newchannel);
-                return false;
-            }
+        if (checkChannel(channel, actualchannel)){
+          leaveChannel(actualchannel);
+          joinChannel(channel);
         }
-    });
-
-    // When a message is sent, call 'send message' function from server
-    document.querySelector('#sendMessage').onsubmit = () => {
-        const message = document.querySelector('#Message').value;
-        const user = localStorage.getItem('nickname');
-            
-        const date = new Date();
-        const messageJSON = {
-            message: message,
-            user: user,
-            date: date
-        }
-        const lastchannel = localStorage.getItem('lastchannel');
-        socket.emit('send message', {'JSON': messageJSON, 'channel': lastchannel});
-        // Clear message form
-        document.querySelector('#Message').value = "";
+      }
+      else {
+        window.alert("The channel already exists");
         return false;
-    };
+      }
+      // Clear input field and disable button again
+      document.querySelector('#Channel').value = '';
+      document.querySelector('#submit').disabled = true;
 
-    // Callback from server for sending messages
-    socket.on('broadcast message', data =>{
-        console.log(data.message.message);
-        console.log(data.username)
-        console.log(data.date)
+      // Stop form from submitting
+      event.preventDefault();  
+    })
+  })
 
-        // Append message to list of messages
-        const li = document.createElement('li');
-        li.innerHTML = `${data}`;
-        document.querySelector('#Messages').append(li);
+  socket.on('new channel', data => {
+    channel = data.channel;
+    channels.push(channel);
+    const li = document.createElement('li');
+    li.setAttribute('class', 'list-group-item active')
+    const a = li.appendChild(document.createElement('a'));        
+    a.innerHTML = channel;
+    a.href = "#";
+    a.addEventListener("click", () => { 
+      actualchannel = localStorage.getItem('lastchannel');
+      newchannel = a.innerHTML;
+      if (checkChannel(actualchannel, newchannel)){
+        leaveChannel(actualchannel);
+        /*socket.on('leaveuser', data => {
+          let li = document.getElementById(data.user);
+          if (li) {
+            li.remove(); 
+          }
+        })*/
+        joinChannel(newchannel);
+                  
+        return false;
+      }    
+    })
+    document.querySelector('#Channels').append(li);
+  });
 
-    });
+  /*socket.on('announcement', data =>{
+    window.alert(data.message);
+  }) */
 
-    function checkChannel(newchannel, oldchannel) {
-        if (newchannel === oldchannel){
-            return false;
+  // When a channel is clicked, connect to that channel
+  document.querySelectorAll('#Channels li a').forEach(function(channel){
+    channels.push(channel.innerHTML);
+    channel.onclick = () =>{
+      //document.querySelector('.list-group-item.active').classList.remove('active');
+      let chanlist = channel.classList;
+      chanlist.add('active');
+      actualchannel = localStorage.getItem('lastchannel');
+      newchannel = channel.innerHTML;
+      console.log(newchannel);
+      if (checkChannel(newchannel, actualchannel)){
+        leaveChannel(actualchannel);
+        joinChannel(newchannel);
+        return false;
+      }
+    }
+  });
+
+  // When a message is sent, call 'send message' function from server
+  document.querySelector('#sendMessage').onsubmit = () => {
+    const message = document.querySelector('#Message').value;
+    const user = localStorage.getItem('nickname');
+    const date = new Date();
+    const messageJSON = {
+      message: message,
+      user: user,
+      date: date
+    }
+    const lastchannel = localStorage.getItem('lastchannel');
+    socket.emit('send message', {'JSON': messageJSON, 'channel': lastchannel});
+    // Clear message form
+    document.querySelector('#Message').value = "";
+    return false;
+  };
+
+  // Callback from server for sending messages
+  socket.on('broadcast message', data =>{
+    console.log(data)
+    appendMessage(data.message.message, data.username, data.date, data.messageID)
+  });
+
+  function appendMessage(message, user, date, messageID) {
+      const a = document.createElement('a');
+      a.classList.add('list-group-item', 'list-group-item-action', 'message');
+      document.querySelector('#SingleMessage').append(a);
+      const div = document.createElement('div');
+      div.classList.add('d-flex', 'w-100' ,'justify-content-between');
+      const alist = document.querySelectorAll('#SingleMessage a');
+      const last = alist[alist.length - 1];
+      last.append(div)
+      const h5 = document.createElement('h5');
+      h5.classList.add('mb-1');
+      console.log(message.username)
+      if (user === localStorage.getItem('nickname')) {
+        h5.innerHTML = 'You wrote:'
+        const button = document.createElement('button');
+        button.classList.add('btn', 'btn-secondary', 'btn-sm');
+        button.innerHTML = 'delete';
+        button.onclick = () => {
+          socket.emit('delete message',{'messageID': messageID});
+          console.log(button.parentElement.parentElement)
+          button.parentNode.parentNode.style.animationPlayState = 'running';
+          button.parentNode.parentNode.addEventListener('animationend', () =>  {
+            button.parentElement.parentElement.remove();
+          });
         }
-        return true;
-    }
+        last.firstChild.append(h5);
+        last.firstChild.append(button);
+      }
+      else {
+        h5.innerHTML = `${user} wrote:`
+        last.firstChild.append(h5);
+      }
+      const p = document.createElement('p');
+      p.classList.add('mb-1');
+      p.innerHTML= `${message}`
+      last.append(p);
+      const small = document.createElement('small');
+      small.classList.add('text-muted');
+      small.innerHTML = `${date}`;
+      last.append(small);
+  }
 
-    function joinChannel(channel) {
-        socket.emit('join',{"channel":channel, "username":username});
-        localStorage.setItem('lastchannel', channel);
-        document.querySelector('#channelName').innerHTML = channel;    
-        users = []
+  function checkChannel(newchannel, oldchannel) {
+    if (newchannel === oldchannel){
+      return false;
     }
+    return true;
+  }
 
-    function leaveChannel(channel) {
-        console.log('leave')
-        socket.emit('leave',{"channel":channel, "username":username});   
-    }
-   }) 
+  function joinChannel(channel) {
+    socket.emit('join',{"channel":channel, "username":username});
+    localStorage.setItem('lastchannel', channel);
+    document.querySelector('#channelName').innerHTML = channel;    
+    users_channel = []
+  }
+
+  function leaveChannel(channel) {
+    socket.emit('leave',{"channel":channel, "username":username});   
+  }
+}) 
